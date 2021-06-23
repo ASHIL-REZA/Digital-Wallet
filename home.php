32889c47875b66256f810761eba70d9bfaed26ef
ASHIL-REZA <ashilrezasporsho@gmail.com>
2021-06-23 13:46:07 +0600

    first commit

---
+++ home.php
@@ -0,0 +1,10 @@
+<!doctype html>
+	<head>
+		<title>PHP</title>
+		<body>
+			<div class="container" style="width: 600px;">
+			<h1>Home</h1>
+			<h2>Digital Wallet</h2>
+			<h3>Fund transfer</h3> 
+		</body>
+	</head>
